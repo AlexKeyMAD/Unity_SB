cd912fcd6abe1167ed479cd36558b478f64a72cc
Alexander <49525149+AlexKeyMAD@users.noreply.github.com>
2024-02-24 00:53:19 +0600

    end task 11_1

--- Jun/Task_11_1/Assets/Scripts/PausePrefabScript.cs
+++ Jun/Task_11_1/Assets/Scripts/PausePrefabScript.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PausePrefab : MonoBehaviour
 {
     public GameObject panel;
+    public Text levelText;
 
     void Start()
     {
         panel.SetActive(false);
+
+        levelText.text = $"Уровень {SceneManager.GetActiveScene().buildIndex.ToString()}";
     }
 
     public void GamePause()
@@ -16,5 +21,17 @@ public class PausePrefab : MonoBehaviour
         Time.timeScale = 0;
 
         panel.SetActive(true);
+    }
+
+    public void BackToGame()
+    {
+        Time.timeScale = 1;
+
+        panel.SetActive(false);
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
