854781f8cf8dc04aa3bc478c2b004fcad2d32afb
Alexandr Gussakov <it_np@mail.ru>
2024-03-04 12:46:02 +0500

    update task 13

---
+++ Jun/Task_13/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    [SerializeField, Range(0, 10)] private int speed = 3;
+    private Vector3 move;
+
+    private void Awake()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horisont = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        move = new Vector3(horisont, 0, vertical).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        playerRB.AddForce(move * speed);
+    }
+
+
+#if UNITY_EDITOR
+    [ContextMenu("Reset values")]
+    public void ResetValues()
+    {
+        speed = 3;
+    }
+#endif
+}
